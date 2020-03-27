f04f2717c7f187dbae7083de362967f9358df41c
omar <elbakaliomar9@gmail.com>
2020-03-27 10:15:51 -0700

    Implemented Image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,10 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'vue js')
     end
     scope :ruby_on_rails, -> { where(subtitle: 'Ruby On Rails') }
+
+    after_initialize :set_defaults
+    def set_defaults
+        self.main_image ||= "https://via.placeholder.com/600x400.png?text=Visit+WhoIsHostingThis.com+Buyers+Guide"
+        self.thub_image ||= "https://via.placeholder.com/350x200.png?text=Visit+WhoIsHostingThis.com+Buyers+Guide"
+    end
 end
