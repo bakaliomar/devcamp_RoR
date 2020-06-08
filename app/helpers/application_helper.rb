cca039d9bc41e2e6895fed6352a659f6e608cbd8
Omar <elbakaliomar9@gmail.com>
2020-06-08 16:45:44 +0100

    Implemented nav bar for blog layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         if !current_user.is_a?(GuestUser)
             link_to "logout", destroy_user_session_path, method: :delete, class: style
         else 
-            (link_to "login", new_user_password_path, class: style) + " ".html_safe + 
+            (link_to "login", new_user_session_path, class: style) + " ".html_safe + 
             (link_to "register", new_user_registration_path, class: style) 
         end 
     end
