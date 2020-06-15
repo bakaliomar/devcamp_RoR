75ba78931e3bfeb5a458d9bd0bb61ebbc85756b0
Omar <elbakaliomar9@gmail.com>
2020-06-15 15:27:40 +0100

    Refactored alerts into helpers methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,4 +50,13 @@ module ApplicationHelper
     def Active? path
         "active" if current_page? path
     end
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+        if alert
+            alert_generator alert
+        end
+    end
+    def alert_generator msg
+         js add_gritter(msg, title: "Omar El Bakali Portfolio", sticky: false)
+    end
 end
