a98d41fc63e7df8b3f99f42891ad8f69d8c4a08f
Omar <elbakaliomar9@gmail.com>
2020-06-25 02:42:40 +0100

    Fix some bugs in social tool for twitter

--- lib/social_tool.rb
+++ lib/social_tool.rb
@@ -1,13 +1,14 @@
-module SocialTool
+ module SocialTool
     def self.twitter_search
-        client = Twitter::Streaming::Client.new do |config|
-            config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
-            config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
-            config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
-            config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
-          end
-        client.saerch('#rails', result_type: 'recent').take(6).collect do |tweet|
-            "#{tweet.user.screen_name}: #{tweet.text}"
-        end
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
+        config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
+        config.access_token        = ENV.fetch("TWITTER_ACCESS_TOKEN")
+        config.access_token_secret = ENV.fetch("TWITTER_ACCESS_SECRET")
+      end
+  
+      client.search("#coding", result_type: 'recent').take(6).collect do |tweet|
+        "#{tweet.user.screen_name}: #{tweet.text}"
+      end
     end
 end
